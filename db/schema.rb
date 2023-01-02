5692727d155401bc53364e8eb2ee55f1cebbd89d
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 13:55:34 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_103649) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_104852) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -18,4 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_103649) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.integer "role_id"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_users_on_role_id"
+  end
+
+  add_foreign_key "users", "roles"
 end
