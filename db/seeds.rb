e7b2c26a9e1c5076285bfaa578472c5079b5a1b9
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 13:47:35 +0300

    add  role seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+roles =
+  Role.create(
+    [
+      {
+        name: "Collector",
+        description:
+          "An individual responsible for collecting waste and taking it to drop points"
+      },
+      {
+        name: "Disposer",
+        description:
+          "An individual responsible for disposing waste and buying products in the market."
+      },
+      {
+        name: "Farmer",
+        description:
+          "Responsible for buying compost and selling organics in the market"
+      },
+      {
+        name: "Partner",
+        description:
+          "Responsible for disposing waste and buying inorganic waste at a premium service"
+      }
+    ]
+  )
